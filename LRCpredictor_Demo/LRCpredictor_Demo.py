3dc7ed1b4fe0946bdcd5ff5953875b30810e24b2
Huangxiaojie2024 <xj_huang@163.com>
2025-12-31 10:30:12 +0800

    Update LRCpredictor_Demo.py

--- LRCpredictor_Demo/LRCpredictor_Demo.py
+++ LRCpredictor_Demo/LRCpredictor_Demo.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
 ==============================================================================
-LRCpredictor: Lactation Risk Classification Predictor
+LRCpredictor
 ==============================================================================
 
 Supplementary Code for Manuscript:
